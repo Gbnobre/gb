4cb17c225a595b11c2f2a84cdca45a1ec5e6174f
Gbnobre <166552655+Gbnobre@users.noreply.github.com>
2024-05-14 17:03:49 -0300

    Add files via upload

---
+++ frames2.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+class MinhaGUI:
+ def __init__(self):
+     
+  # Criando a janela principal
+  self.janela_principal = Tk()
+  
+  # Criando os frames
+  self.frame_cima = Frame(self.janela_principal, bg="white", height=70, width=400)
+  self.frame_baixo = Frame(self.janela_principal, bg='red', height=70, width=400)
+  
+  # Posicionando o frame
+  self.frame_cima.pack()
+  self.frame_baixo.pack()
+
+  # Fazer o Tkinter exibir o looping da janela
+  mainloop()
+
+minha_gui = MinhaGUI()
